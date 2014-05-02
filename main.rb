94c4574df1850ec19d82710047068ce81b9776e2
Alejandro <alu0100699715@ull.edu.es>
2014-05-02 16:03:23 +0100

    Intentando añadir base de datos para cada usuario

--- main.rb
+++ main.rb
@@ -120,7 +120,7 @@ post '/save' do
       pp c
       flash[:notice] = 
         %Q{<div class="Salgo de save"</div>}
-      redirect to '/'#+name
+      redirect to '/'+user.username+'/'+name
     end
   else
     flash[:notice] = 
